18860566e4c593f369d55ecf2cf8488e4df951da
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-07 09:56:30 +0700

    feat(onboarding): implement Onboarding screen

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -15,17 +15,102 @@ struct OnboardingView: View {
   // MARK: Body
 
   var body: some View {
-    VStack(spacing: 20) {
-      Text("Onboarding")
-        .font(.largeTitle)
+    ZStack {
+      Color("ColorBlue")
+        .ignoresSafeArea(.all, edges: .all)
 
-      Button(action: {
-        // action
-        isOnboardingViewActive = false
-      }) {
-        Text("Start")
-      }
-    } //: VStack
+      VStack(spacing: 20) {
+        // MARK: Header
+
+        Spacer()
+        VStack(spacing: 0) {
+          Text("Share.")
+            .font(.system(size: 60))
+            .fontWeight(.heavy)
+            .foregroundColor(.white)
+
+          Text("""
+          It's not how much we give but
+          how much love we put into giving.
+          """)
+          .font(.title3)
+          .fontWeight(.light)
+          .foregroundColor(.white)
+          .multilineTextAlignment(.center)
+          .padding(.horizontal, 10)
+        } //: Header
+
+        // MARK: Center
+
+        ZStack {
+          ZStack {
+            Circle()
+              .stroke(.white.opacity(0.2), lineWidth: 40)
+              .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+              .stroke(.white.opacity(0.2), lineWidth: 80)
+              .frame(width: 260, height: 260, alignment: .center)
+          } //: ZStack
+          Image("character-1")
+            .resizable()
+            .scaledToFit()
+        } //: Center
+
+        Spacer()
+
+        // MARK: Footer
+
+        ZStack {
+          // Parts of the custom button
+
+          // 1. background (static)
+          Capsule()
+            .fill(Color.white.opacity(0.2))
+
+          Capsule()
+            .fill(Color.white.opacity(0.2))
+            .padding(8)
+
+          // 2. call-to-action (static)
+
+          Text("Get Started")
+            .font(.system(.title3, design: .rounded))
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+            .offset(x: 20)
+
+          // 3. capsule (dynamic width)
+          HStack {
+            Capsule()
+              .fill(Color("ColorRed"))
+              .frame(width: 80)
+            Spacer()
+          }
+
+          // 4. circle (draggable)
+          HStack {
+            ZStack {
+              Circle()
+                .fill(Color("ColorRed"))
+              Circle()
+                .fill(.black.opacity(0.15))
+                .padding(8)
+              Image(systemName: "chevron.right.2")
+                .font(.system(size: 24, weight: .bold))
+            }
+            .foregroundColor(.white)
+            .frame(width: 80, height: 80, alignment: .center)
+            .onTapGesture {
+              isOnboardingViewActive = false
+            }
+
+            Spacer()
+          } //: HStack
+        } //: Footer
+        .frame(height: 80, alignment: .center)
+        .padding()
+      } //: VStack
+    } //: ZStack
   }
 }
 
