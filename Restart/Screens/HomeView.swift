99f44af3f8760c36944d87316c291dd2965e6e75
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-07 11:54:39 +0700

    feat(home): implement Home screen

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -16,15 +16,50 @@ struct HomeView: View {
 
   var body: some View {
     VStack(spacing: 20) {
-      Text("Home")
-        .font(.largeTitle)
+      // MARK: Header
+
+      Spacer()
+
+      ZStack {
+        Circle()
+          .stroke(.black.opacity(0.1), lineWidth: 40)
+          .frame(width: 260, height: 260, alignment: .center)
+        Circle()
+          .stroke(.black.opacity(0.1), lineWidth: 80)
+          .frame(width: 260, height: 260, alignment: .center)
+
+        Image("character-2")
+          .resizable()
+          .scaledToFit()
+          .padding()
+      } //: Header
+
+      // MARK: Center
+
+      Text("The time that leads to mastery is dependent on the intensity of our focus.")
+        .font(.title3)
+        .fontWeight(.light)
+        .foregroundColor(.secondary)
+        .multilineTextAlignment(.center)
+        .padding()
+
+      // MARK: Footer
+
+      Spacer()
 
       Button(action: {
-        // action
         isOnboardingViewActive = true
       }) {
+        Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+          .imageScale(.large)
+
         Text("Restart")
+          .font(.system(.title3, design: .rounded))
+          .fontWeight(.bold)
       }
+      .buttonStyle(.borderedProminent)
+      .buttonBorderShape(.capsule)
+      .controlSize(.large)
     } // VStack
   }
 }
